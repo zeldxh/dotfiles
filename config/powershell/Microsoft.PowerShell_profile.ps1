6a81105b8e153d25b6daad99d4b895bd606f2afc
Zel <783613+zeldxh@users.noreply.github.com>
2026-09-24 23:19:49 -0600

    feat(powershell): add PSReadLine syntax colors and history suggestions

--- config/powershell/Microsoft.PowerShell_profile.ps1
+++ config/powershell/Microsoft.PowerShell_profile.ps1
@@ -30,3 +30,25 @@ function dev { Set-Location "$HOME\dev" }
 
 # 7z: alias for 7-Zip
 Set-Alias 7z "C:\Program Files\7-Zip\7z.exe"
+# Syntax colors as you type (same palette as the VS Code theme) and history suggestions
+if (Get-Module -ListAvailable PSReadLine) {
+    $c = { param($hex) $r,$g,$b = [Convert]::ToInt32($hex.Substring(0,2),16),[Convert]::ToInt32($hex.Substring(2,2),16),[Convert]::ToInt32($hex.Substring(4,2),16); "`e[38;2;$r;$g;${b}m" }
+    Set-PSReadLineOption -Colors @{
+        Command            = & $c '6a9fb5'   # blue
+        Parameter          = & $c 'f4bf75'   # yellow
+        String             = & $c '90a959'   # green
+        Number             = & $c 'f4bf75'
+        Variable           = & $c 'd8d8d8'
+        Operator           = & $c 'aa759f'   # magenta
+        Keyword            = & $c 'aa759f'
+        Type               = & $c 'f4bf75'
+        Member             = & $c 'd06060'   # red
+        Comment            = & $c '7f7f7f'
+        Error              = & $c 'd06060'
+        InlinePrediction   = & $c '6b6b6b'
+        Default            = & $c 'd8d8d8'
+    }
+    Set-PSReadLineOption -PredictionSource History -PredictionViewStyle InlineView -ErrorAction SilentlyContinue
+    Set-PSReadLineKeyHandler -Key UpArrow   -Function HistorySearchBackward
+    Set-PSReadLineKeyHandler -Key DownArrow -Function HistorySearchForward
+}
